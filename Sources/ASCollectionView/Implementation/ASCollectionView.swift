ea584ad3c8408b6fcbd9942676c905a6d3647406
Jean Vinge <jean.vinge@rubylife.com>
2021-05-04 16:56:38 -0300

    fix scroll

--- Sources/ASCollectionView/Implementation/ASCollectionView.swift
+++ Sources/ASCollectionView/Implementation/ASCollectionView.swift
@@ -519,11 +519,10 @@ public struct ASCollectionView<SectionID: Hashable>: UIViewControllerRepresentab
 		func scrollToPosition(_ scrollPosition: ASCollectionViewScrollPosition, animated: Bool = false)
 		{
             let notch = UIApplication.shared.keyWindow?.safeAreaInsets.top ?? 0
-            print("notch \(notch)")
 			switch scrollPosition
 			{
 			case .top, .left:
-                collectionViewController?.collectionView.setContentOffset(.init(x: 0, y: notch), animated: animated)
+                collectionViewController?.collectionView.setContentOffset(.init(x: 0, y: -notch), animated: animated)
 			case .bottom:
 				guard let maxOffset = collectionViewController?.collectionView.maxContentOffset else { return }
 				collectionViewController?.collectionView.setContentOffset(.init(x: 0, y: maxOffset.y), animated: animated)
